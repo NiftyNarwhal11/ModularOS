11b40585fce2e632858b9ba28c1fbe4ba44d84ae
NiftyNarwhal11 <153830534+NiftyNarwhal11@users.noreply.github.com>
2024-05-02 09:44:22 -0400

    Update Rednet.lua
    added side question
    
    Signed-off-by: NiftyNarwhal11 <153830534+NiftyNarwhal11@users.noreply.github.com>

--- ModuleInstallers/Rednet.lua
+++ ModuleInstallers/Rednet.lua
@@ -1,1 +1,9 @@
 shell.run("wget https://raw.githubusercontent.com/NiftyNarwhal11/ModularOS/v0.0.1/.Modules/RednetModule?token=GHSAT0AAAAAACRUSC4CSNXKXCNDHTQJCP5EZRTRTJQ /startup/RednetModule")
+settings.define("MOS.rednet.modemside", {
+    description = "The side for the rednet modules modem",
+    default = "back",
+    type = string,
+})
+write("What is the side your modem is on? >")
+local side = read()
+settings.set("MOS.rednet.modemside", side)
