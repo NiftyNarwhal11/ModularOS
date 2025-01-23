3f10e0dace8587077adf235c065e68338a39919b
NiftyNarwhal11 <153830534+NiftyNarwhal11@users.noreply.github.com>
2025-01-23 16:38:39 -0500

    Update STARTUP.lua
    Signed-off-by: NiftyNarwhal11 <153830534+NiftyNarwhal11@users.noreply.github.com>

--- startup/STARTUP.lua
+++ startup/STARTUP.lua
@@ -1,9 +1,9 @@
 shell.exit()
 shell.run( "set motd.enable false" )
 shell.run( "set shell.allow_disk_startup false" )
-local version = "1.0.3"
+local version = tonumber("1.04")
 local latestVersion = http.get("https://raw.githubusercontent.com/NiftyNarwhal11/ModularOS/refs/heads/main/latestVersion")
-local lversion = latestVersion.readAll()
+local lversion = latestVersion.readLine()
 settings.load(".settings")
 if settings.get("MOS.auto-update.enabled") == true then
 if lversion > version then
